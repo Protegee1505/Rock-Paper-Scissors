a74bb18491ea3a60fee27d4f00147da2a4160ab7
Protegee <kujri111@gmail.com>
2023-01-01 02:48:03 +0700

    Add Rock Paper Scissors game to index.js

---
+++ index.js
@@ -0,0 +1,70 @@
+//This Function return randomly Rock, Paper, Scissors
+let getComputerChoice = () => {
+  let choices = ["Rock", "Paper", "Scissors"]
+  let randomIndex = Math.floor(Math.random() * choices.length)
+  return randomStr = choices[randomIndex]
+}
+
+// This variable contain randomly generate Rock, Paper, Scissors
+let computerSelection = getComputerChoice()
+
+// This variable accept Player input and make it lower case
+let playerSelection = window.prompt('Enter').toLocaleLowerCase()
+
+
+// This Function takes randomly generate Rock, Paper, Scissors and Player input and log the result on the console
+let playRound  = (computerSelection,playerSelection) => {
+  if (playerSelection === "rock" && computerSelection === "Rock") {
+    return `You choose Rock the Computer choose ${computerSelection},Tie`
+  } else if (playerSelection === "rock" && computerSelection === "Paper"){
+    return `You choose Rock the Computer choose ${computerSelection},You Loose`
+  } else if (playerSelection === "rock" && computerSelection === "Scissors") {
+    return `You choose Rock the Computer choose ${computerSelection},You Win`
+
+
+
+  } else if (playerSelection === "paper" && computerSelection === "Rock") {
+    return `You choose Paper the Computer choose ${computerSelection},You Win`
+  } else if (playerSelection === "paper" && computerSelection === "Paper"){
+    return `You choose Paper the Computer choose ${computerSelection},Tie`
+  } else if (playerSelection === "paper" && computerSelection === "Scissors") {
+    return `You choose Paper the Computer choose ${computerSelection},You Loose`
+
+
+
+  } else if (playerSelection === "scissors" && computerSelection === "Rock") {
+    return `You choose Scissors the Computer choose ${computerSelection},You Loose`
+  } else if (playerSelection === "scissors" && computerSelection === "Paper"){
+    return `You choose Scissors the Computer choose ${computerSelection},You Win` 
+  } else if (playerSelection === "scissors" && computerSelection === "Scissors") {
+    return `You choose Scissors the Computer choose ${computerSelection},Tie`
+}
+};
+
+
+//This Function loop through playRound 5 times and log the result of the game
+let game = () => {
+  let player = 0;
+  let comp = 0;
+  for (let i = 0; i < 5; i++ ) {
+    playRound(computerSelection,playerSelection)
+    computerSelection = getComputerChoice();
+    console.log(playRound(computerSelection,playerSelection))
+    if (playRound(computerSelection,playerSelection).includes("Win")) {
+      player++
+    }else if (playRound(computerSelection,playerSelection).includes("Loose")){
+      comp++
+    }
+  }
+  if(player < comp) {
+    console.log(`You ${player}: Computer ${comp}: Computer Win!`)
+  }else if (player > comp) {
+    console.log(`You ${player}: Computer ${comp}: You Win!`)
+  }else {
+    console.log(`You ${player}: Computer ${comp}: Tie!`)
+  }
+}
+
+game()
+
+
