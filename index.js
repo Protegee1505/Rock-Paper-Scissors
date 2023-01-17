06c8884984ab6be0d1c26abec86b2822953bc54c
Protegee <kujri111@gmail.com>
2023-01-18 01:51:20 +0700

    Add a pop-up box declare the winner and play again button

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-//This Function return randomly "Rock" or "Paper" or "Scissors"
 let getComputerChoice = () => {
   const compChoices = ["Rock", "Paper", "Scissors"];
   let randomIndex = Math.floor(Math.random() * compChoices.length);
@@ -28,14 +27,9 @@ let playerScore = 0;
 let computerScore = 0;
 
 let playerSelection = (playerChoice) => {
-  resetScore();
-
-  let resultOneRound = playRound(getComputerChoice(), playerChoice);
+  const resultOneRound = playRound(getComputerChoice(), playerChoice);
   resultDiv.innerHTML = resultOneRound;
 
-  if (playerScore >= 0 || computerScore >= 0) {
-    finalScore.textContent = "";
-  }
   if (resultOneRound.includes("win")) {
     playerScore += 1;
     playerScoreDiv.textContent = "You: " + playerScore;
@@ -48,13 +42,41 @@ let playerSelection = (playerChoice) => {
 };
 
 let checkWin = () => {
+  const winnerTextBox = document.querySelector(".winner-text");
+
   if (playerScore >= 5) {
-    finalScore.style.color = "green";
-    finalScore.textContent = "You have 5 points. Win!";
+    showWinBox();
+    winnerTextBox.style.color = "green";
+    winnerTextBox.textContent = "You have 5 points. Win!";
   } else if (computerScore >= 5) {
-    finalScore.style.color = "red";
-    finalScore.textContent = "Computer have 5 points. Loose!";
+    showWinBox();
+    winnerTextBox.style.color = "red";
+    winnerTextBox.textContent = "Computer have 5 points. Loose!";
   }
+};
+
+let showWinBox = () => {
+  const showWinBox = document.querySelector(".show-winner");
+  const resetScoreBtn = document.querySelector(".playAgain-btn");
+  const playerScoreText = document.querySelector(".player-score");
+  const computerScoreText = document.querySelector(".computer-score");
+  showWinBox.style.display = "block";
+
+  if (playerScore > computerScore) {
+    playerScoreText.style.color = "green";
+    computerScoreText.style.color = "grey";
+  } else if (playerScore < computerScore) {
+    computerScoreText.style.color = "red";
+    playerScoreText.style.color = "grey";
+  }
+
+  resetScoreBtn.addEventListener("click", (e) => {
+    resetScore();
+    showWinBox.style.display = "none";
+  });
+
+  playerScoreText.textContent = "You : " + playerScore;
+  computerScoreText.textContent = "Computer :" + computerScore;
 };
 
 let resetScore = () => {
@@ -71,19 +93,19 @@ let playRound = (computerSelection, playerSelection) => {
     case playerSelection === computerSelection:
       return `You select ${playerSelection} the Computer select ${computerSelection}, Tie`;
     case playerSelection === "Rock" && computerSelection === "Paper":
-      return `You select Rock, the Computer selects Paper, You lose.`;
+      return `You select Rock, the Computer selects Paper, You lose the round.`;
     case playerSelection === "Rock" && computerSelection === "Scissors":
-      return `You select Rock, the Computer selects Scissors, You win.`;
+      return `You select Rock, the Computer selects Scissors, You win the round.`;
 
     case playerSelection === "Paper" && computerSelection === "Rock":
-      return `You select Paper, the Computer selects Rock, You win.`;
+      return `You select Paper, the Computer selects Rock, You win the round.`;
     case playerSelection === "Paper" && computerSelection === "Scissors":
-      return `You select Paper, the Computer selects Rock, You lose.`;
+      return `You select Paper, the Computer selects Rock, You lose the round.`;
 
     case playerSelection === "Scissors" && computerSelection === "Rock":
-      return `You select Paper, the Computer selects Rock, You lose.`;
+      return `You select Paper, the Computer selects Rock, You lose the round.`;
     case playerSelection === "Scissors" && computerSelection === "Paper":
-      return `You select Paper, the Computer selects Rock, You win.`;
+      return `You select Paper, the Computer selects Rock, You win the round.`;
     default:
       console.log("Try Again");
   }
